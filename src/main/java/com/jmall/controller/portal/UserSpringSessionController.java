15fc348ad8667b1526020af00378dc6c5b421760
akeal <jy_tian@foxmail.com>
2020-12-09 19:20:31 +0800

    SpringSession-Redis

---
+++ src/main/java/com/jmall/controller/portal/UserSpringSessionController.java
@@ -0,0 +1,77 @@
+package com.jmall.controller.portal;
+
+import com.jmall.common.Const;
+import com.jmall.common.ResponseCode;
+import com.jmall.common.ServerResponse;
+import com.jmall.pojo.User;
+import com.jmall.service.IUserService;
+import com.jmall.util.CookieUtil;
+import com.jmall.util.JsonUtil;
+import com.jmall.util.RedisShardedPoolUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *前台controller
+ */
+@Controller
+@RequestMapping("/user/springSession")
+public class UserSpringSessionController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    /**
+     * 用户登录
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "login.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<User> login(String username, String password, HttpSession session) {
+        ServerResponse response = iUserService.login(username,password);
+        if (response.isSuccess()) {
+            session.setAttribute(Const.CURRENT_USER,response.getData());
+        }
+        return response;
+    }
+
+    /**
+     * 退出
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "logout.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpSession session) {
+        session.removeAttribute(Const.CURRENT_USER);
+        return ServerResponse.createBySuccess();
+    }
+
+    /**
+     * 获取用户信息
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "get_user_info.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<User> getUserInfo(HttpSession session) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user != null) {
+            return ServerResponse.createBySuccess(user);
+        }
+        return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户信息");
+    }
+
+
+}
